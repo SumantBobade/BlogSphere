c00cd84e63611ab8f01c8c9c972c4ff6524b508b
Sumant Bobade <167948550+SumantBobade@users.noreply.github.com>
2025-03-30 14:41:22 +0530

    Add functionality to the Sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,54 +1,115 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  }
+  // console.log(formData);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.')
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate('/sign-in');
+      }
+      
+    } catch (error) { 
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
-    <div className='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* Left */}
-        <div className='flex-1'>
-        <Link
-        to="/"
-        className=" font-bold dark:text-white text-4xl">
-        BLog
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Sphere
-        </span>
+        <div className="flex-1">
+          <Link to="/" className=" font-bold dark:text-white text-4xl">
+            BLog
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Sphere
+            </span>
           </Link>
-          <p className='text-sm mt-5'>
-          Unlock exclusive insights—log in to explore my world of tech, design, and innovation!
+          <p className="text-sm mt-5">
+            Unlock exclusive insights—log in to explore my world of tech,
+            design, and innovation!
           </p>
         </div>
 
         {/* Right */}
-        <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
-            <Label htmlFor="username" value="Your username" />
-              <TextInput type='text' placeholder='Username' id='username' />
+              <Label htmlFor="username" value="Your username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label htmlFor="email" value="Your email" />
-              <TextInput type='email' placeholder='name@company.com' id='email' />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label htmlFor="password" value="Your password" />
-              <TextInput type='password' placeholder='Password' id='password' />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button className="bg-gradient-to-r from-purple-500 to-pink-500 text-white hover:bg-gradient-to-l focus:ring-purple-200 dark:focus:ring-purple-800" type='submit'>
-             Sign Up
-            </Button>
+            <Button
+              className="bg-gradient-to-r from-purple-500 to-pink-500 text-white hover:bg-gradient-to-l focus:ring-purple-200 dark:focus:ring-purple-800"
+              type="submit" disabled={loading}
+            >{
+                loading ? (
+                  <>
+                    <Spinner aria-label="Spinner button example" size="sm" light />
+                    <span className="pl-3">Loading...</span>
+                  </>
+                ) : 'Sign Up'
+              }</Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
-            <Link to='/sign-in' className='text-blue-500'>
+            <Link to="/sign-in" className="text-blue-500">
               Sign In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
-
       </div>
     </div>
-  )
+  );
 }
