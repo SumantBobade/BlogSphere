4d956236a01958dfa80d61ab60eca61e99cbdf46
Sumant Bobade <167948550+SumantBobade@users.noreply.github.com>
2025-04-01 21:42:46 +0530

    Update Header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import { Button, TextInput } from 'flowbite-react';
+import { Avatar, Button, DrawerItems, Dropdown, DropdownDivider, DropdownHeader, DropdownItem, TextInput } from 'flowbite-react';
 import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector(state => state.user);
   
   return (
     <Navbar className="border-b-2 bg-white">
@@ -36,9 +38,33 @@ export default function Header() {
         <Button className="w-12 h-10 sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/sign-in">
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={<Avatar
+              alt='user'
+              img={currentUser.profilePicture}
+              rounded
+            />
+            }>
+            <DropdownHeader>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+            </DropdownHeader>
+            <Link to={'/dashboard?tab=profile'}>
+              <DropdownItem>Profile</DropdownItem>
+            </Link>
+
+            <DropdownDivider />
+            <DropdownItem>SignOut</DropdownItem>
+          </Dropdown>
+        ):(
+          <Link to="/sign-in">
           <Button className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white hover:bg-gradient-to-bl focus:ring-cyan-300 dark:focus:ring-cyan-800 outline" >Sign In</Button>
-        </Link>
+          </Link>
+        )}
+        
         <NavbarToggle />
       </div>
       
